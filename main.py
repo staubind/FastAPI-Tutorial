a8ac1da11dba52fa9a0e99bf21f130097111bf93
Dan St. Aubin <staubindj@gmail.com>
2021-11-09 16:00:30 -0600

    finished query params and looking into dealing with the body - onto post requests

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 # create an enum class
 class ModelName(str, Enum):
@@ -24,12 +25,43 @@ async def get_model(model_name: ModelName):
 async def root():
     return {"message": "Hello World"}
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+# params not part of path params are query params
+# going to: http://127.0.0.1:8000/items/?skip=1&limit=1
+# will return [{"item_name": "Bar"}]
+# query params initially string, but using typing converts them and validates them
+@app.get("/items/")
+async def read_item(skip: int=0, limit: int=10):
+    return fake_items_db[skip : skip + limit]
+
+# can set optional parameters by setting their default val to None w/ Optional[type]
 @app.get('/items/{item_id}')
-async def read_item(item_id: int):
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
     print(item_id, type(item_id))
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
     # note that 3 is automatically parsed as int rather than string
     # also provides error if you provide a float or non-coercable string
-    return {"item_id": item_id}
+    return item
+
+# if you don't provide default value and go to http://127.0.0.1:8000/item/foo-item you'll get an error in the json
+# but if you use http://127.0.0.1:8000/item/foo-item?needy=sooooneedy it comes through
+@app.get('/item/{item_id}') # used /item -> singular to keep all endpoints available in this tutorial
+async def read_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+@app.get('/ite/{item_id}')
+async def read_item(
+    item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
 
 # paths are evaluated in order, so make sure statics are before dynamic:
 @app.get("/users/me")
@@ -41,7 +73,27 @@ async def read_user_me():
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
+# you can also use multiple parameters and multiple query params in the same endpoint
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
 # :path tells it that it should match any path
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+
+
+
