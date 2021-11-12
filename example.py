b1784e1d1ea03db561987708dccf65dd87996164
Dan St. Aubin <staubindj@gmail.com>
2021-11-12 14:26:25 -0600

    finished seeing how to add examples to the api

---
+++ example.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, Body
+from typing import Optional
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+# how to declare examples for documentation purposes
+class Item(BaseModel):
+    name: str = Field(..., example="Foo")
+    description: Optional[str] = Field(None, example="A very nice Item")
+    price: float = Field(..., example=35.4)
+    tax: Optional[float] = Field(None, example=3.2)
+
+    # if you use the above where you define an example attribute inside
+    # a pydantic model, there's no need to create a config here.
+    # example will be used ONLY for documentation
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, 
+    item: Item = Body(
+        ...,
+        examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A **normal** item works correctly.",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "value": {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            },
+        },
+    ),
+):
+    results = {"item_id": item_id, "item": item} 
+    return results
+
+# to have multiple examples
