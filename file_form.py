a9cb3a635d2bf3aa34423cea2e5bcb0e2780ac70
Dan St. Aubin <staubindj@gmail.com>
2021-11-14 21:47:23 -0600

    brief files and forms overview - mostly focused on encoding of the body of the request

---
+++ file_form.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, File, UploadFile, Form
+
+app = FastAPI()
+
+@app.post("/files/")
+async def create_files(
+    file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)
+):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+    }
