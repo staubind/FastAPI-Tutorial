405309401124d510a1abb54da70e9112f8833fe6
Dan St. Aubin <staubindj@gmail.com>
2021-11-20 13:03:05 -0600

    added endpoints and module file

--- sql/main.py
+++ sql/main.py
@@ -21,6 +21,7 @@ def get_db():
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # sqlalchemy isn't compatible w/ await, so we define it as non-async
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -48,3 +49,6 @@ def create_item_for_user(
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(*db, skip=skip, limit=limit)
     return items
+    # note that the object returned is a list of sqlalchemy models, not pydantic models as is 
+    # called for by the response_model declaration
+    # pydantic models with orm_mode = True automatically parse these sqlalchemy models
